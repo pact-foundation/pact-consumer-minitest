e6613a3facd208da89ec2ee6f18b553669f5ce14
Beth <beth@bethesque.com>
2015-07-19 08:10:38 +1000

    Added test to ensure RSpec is not loaded

--- spec/lib/pact/consumer/minitest_spec.rb
+++ spec/lib/pact/consumer/minitest_spec.rb
@@ -6,38 +6,40 @@ require './spec/support/test'
 
 describe Pact::Consumer::Minitest do
 
-  before do
-    pact_test.reset
-    pact_test.expects(:pact_spec_hooks).returns(spec_hooks).at_least_once
-    spec_hooks.stubs(:before_all)
-    spec_hooks.stubs(:before_each)
-    spec_hooks.stubs(:after_each)
+  it "does not load RSpec" do
+    assert_equal(nil, defined?(::RSpec))
   end
 
-  let(:spec_hooks) { Pact::Consumer::SpecHooks.new }
-  let(:pact_test) { TestTest.new }
-
-  describe "before_setup" do
-
-    it "ensures SpecHooks.before_all is only called once per suite" do
-      spec_hooks.expects(:before_all)
-      pact_test.before_setup
-      pact_test.before_setup
+  describe "hooks" do
+    before do
+      pact_test.reset
+      pact_test.expects(:pact_spec_hooks).returns(spec_hooks).at_least_once
+      spec_hooks.stubs(:before_all)
+      spec_hooks.stubs(:before_each)
+      spec_hooks.stubs(:after_each)
     end
 
-    it "invokes SpecHooks.before_each" do
-      spec_hooks.expects(:before_each).with('TestTest')
-      pact_test.before_setup
+    let(:spec_hooks) { Pact::Consumer::SpecHooks.new }
+    let(:pact_test) { TestTest.new }
+
+    describe "before_setup" do
+      it "ensures SpecHooks.before_all is only called once per suite" do
+        spec_hooks.expects(:before_all)
+        pact_test.before_setup
+        pact_test.before_setup
+      end
+
+      it "invokes SpecHooks.before_each" do
+        spec_hooks.expects(:before_each).with('TestTest')
+        pact_test.before_setup
+      end
     end
 
+    describe "after_teardown" do
+      it "invokes SpecHooks.after_each" do
+        spec_hooks.expects(:after_each).with('TestTest')
+        pact_test.after_teardown
+      end
+    end
   end
-
-  describe "after_teardown" do
-    it "invokes SpecHooks.after_each" do
-      spec_hooks.expects(:after_each).with('TestTest')
-      pact_test.after_teardown
-    end
-  end
-
 end
-
